419796ff930642a68c87f769864aab795e3239b1
Kerry Lu <kerry.lu@duke.edu>
2021-02-21 17:49:31 -0500

    Done

--- Project1.py
+++ Project1.py
@@ -1,4 +1,5 @@
-import argparse
+# Project1.py
+# Scans input file of language CATANDMOUSE and identifies all tokens and prints their associated information
 
 # initialize symbol table
 symbolTable = {}
@@ -31,99 +32,111 @@ output = [
 
 # scanner
 def findTokens(file):
+    lineCounter = 0
     for line in file:
-        line = line.strip()
-        temp = 0
-        string = ''
-        isComment = False
+        line = line.strip() # remove whitespace
+        start = 0   # initalize the start index of the token string that we want to check
+        string = '' # initialize token string
+        isComment = False   # tracks whether the line is a comment or not
         for x in range(len(line)):
+            # comment check
             if line[x] == '/' and line[x+1] and line[x+1] == '/':
                 isComment = True
+            # skip everything if comment
             if isComment:
                 continue
-            if x == len(line)-1 or line.strip()[x] == ' ':
-                if x == len(line)-1:
-                    string = line[temp:x+1]
-                    temp = x+1
+            # tokens are separated by whitespace, end of line, and end of file
+            if x == len(line)-1 or line.strip()[x] == ' ':  # x is the index of the line that separates tokens
+                if x == len(line)-1:    # x marks end of line
+                    # since x is at the end of the line, we don't need to set an upper bound
+                    string = line[start:]
+                else:   # x marks whitespace
+                    # set upper bound so that string has no whitespace
+                    string = line[start:x]
+                    # we need to update start because we are still iterating through the same line and don't want redundant strings
+                    start = x+1
+            # iterate character by character and check if the string is a token
+            string = string.lower()     # language is case insensitive
+            if isToken(string):         # filters out pure whitespace
+                if not isValidToken(string):    # handles invalid tokens
+                    print('Error, line number ' + str(lineCounter) + ', invalid token: ' + string)
                 else:
-                    string = line[temp:x]
-                    temp = x
-    # iterate character by character and check if the string is a token
-            isVariable = True
-            if isToken(string):
-                if string in keywords:
-                    isVariable = False
-                elif string in punctuationSymbols:
-                    isVariable = False
-                elif 0 <= len(string) <= 3:
-                    isInteger = True
-                    for i in range(len(string)):
-                        if not string[i].isdigit():
-                            isInteger = False
-                    if isInteger:
-                        if string not in symbolTable:
-                            symbolTable[string] = ('integer', string, int(string))
-                    else:
+                    # if string is a keyword, return its type and None for its value
+    
+                    # if string is a punctuation symbol, return its type and None for its value
+
+                    # if string is an integer, enter it into the symbol table.
+                    # the type of an integer is int.
+                    # store the character value and convert the character string to an int and then store the int value
+
+                    # if string is a variable, enter it into the symbol table
+                    # the type of a variable is variable.
+                    # character value is the name of a variable with all letters in lowercase, and its int value is 0 for now.
+
+                    isVariable = True   # tracks if the token is a variable
+                    if string in keywords:  # string is a keyword
+                        isVariable = False
+                    elif string in punctuationSymbols:  # string is a punctuationSymbol
+                        isVariable = False
+                    elif 0 < len(string) <= 3:  # strings made up solely of integers are integer tokens if their length is 3 or less 
+                                                # and variables if length greater than 3
+                        isInteger = True    # tracks if the token is an integer
+                        for i in range(len(string)):
+                            if not string[i].isdigit():     # if any of the characters in the string are not integerss, then the token is not an integer type
+                                isInteger = False 
+                        if isInteger:   # integer types are inserted into the symbol table differently from variable types
+                            if string not in symbolTable:
+                                symbolTable[string] = ('integer', string, int(string))
+                        else:   # if variable
+                            if string not in symbolTable:
+                                symbolTable[string.strip()] = ('variable', string, 0)
+                    else:   # string is a variable
                         if string not in symbolTable:
                             symbolTable[string.strip()] = ('variable', string, 0)
-                else:
-                    if string not in symbolTable:
-                        symbolTable[string.strip()] = ('variable', string, 0)
-                if isVariable:
-                    output.append([symbolTable[string.strip()][0], symbolTable[string.strip()][1], symbolTable[string.strip()][2]])
-                else:
-                    output.append([string.strip(), '', ''])
-                string = ''
+                    if isVariable:  # encompasses variables and integers
+                        output.append([symbolTable[string.strip()][0], symbolTable[string.strip()][1], symbolTable[string.strip()][2]])
+                    else:   # keywords and punctuationSymbols are printed differently than variables and integers
+                        output.append([string.strip(), '', ''])
+                string = '' # reinitialize string
+        lineCounter += 1    # iterate lineCounter
                 
-    return
-         
-    # if string is a keyword, return its type and None for its value
+    return 0
 
-    # if string is a punctuation symbol, return its type and None for its value
-
-    # if string is an integer, enter it into the symbol table.
-    # the type of an integer is int.
-    # store the character value and convert the character string to an int and then store the int value
-
-    # if string is a variable, enter it into the symbol table
-    # the type of a variable is variable.
-    # character value is the name of a variable with all letters in lowercase, and its int value is 0 for now.
-
-    return
 # check if string is a token
 def isToken(s):
-    # TODO: Stack?
-    if s == '':
+    if s.strip() == '': # filters out whitespace
         return False
     return True
-# check if variable name is valid
-def isValidVariableName(s):
-    return
 
-# handle tokens
-def insertToken(file):
-    return
-# Handle Output
+# filters out edge cases
+def isValidToken(s):
+    # TODO: Stack?
+    # punctuation is not alphanumeric so we need to return True before any other checks
+    if s == ';':
+        return True
+    # edge case: if string starts with 0, then the length of the string must be 1
+    if s[0] == 0 and len(s) > 1:
+        return False
+    for ch in s:
+        if not ch.isalnum():    # all characters in valid tokens should be alphabetical or digits
+            return False
+    return True
+
+# Handle formatting of printing the output
+# print out the following information for each token in three columns: 
+# the type of token, the character value, and the integer value. 
+# If the token is not entered into the symbol table, then the character and integer values are left blank.
 def printOutput(output):
-    # print out the following information for each token in three columns: 
-    # the type of token, the character value, and the integer value. 
-    # If the token is not entered into the symbol table, then the character and integer values are left blank.
-    col_width = max(len(str(word)) for row in output for word in row)
+    # sets column width to the length of the longest string 
+    col_width = max(len(str(word)) for row in output for word in row) + 2   # padding
+    # print row by row
     for row in output:
-        print (" ".join(str(word).ljust(col_width) for word in row))
-    return
-# Error Handling
-def handleError():
-    # When an invalid token is found, report it as an error, list the line number the error occured on, and continue processing.
-    # When an invalid token is encountered in the scanner, print an error message indicating the error and the line number, and the token, then continue scanning for the next token.
-    return
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('file', type=argparse.FileType('r'))
-    args = parser.parse_args()
+        print ("".join(str(word).ljust(col_width) for word in row))
+    return 0
 
-    # TODO: Prompt user for the name of CATANDMOUSE program to test
-    findTokens(args.file)
-    print(symbolTable)
-    printOutput(output)
-    # TODO: execute functions on input file
+if __name__ == "__main__":
+    # Prompt user for the name of CATANDMOUSE program to test
+    file = open('./' + input('Enter file name: '), 'r') 
+    findTokens(file)        # find and handle tokens
+    printOutput(output)     # print output
+    file.close()
---
+++ p1.mc
@@ -0,0 +1,6 @@
+// program 1
+size 30 40
+begin
+    cat charlotte 20 21 east ;
+    move charlotte 4 ;
+halt
