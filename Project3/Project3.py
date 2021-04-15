3c317af310128736ec511ec46a6df87502a745f1
Kerry Lu <kerry.lu@duke.edu>
2021-04-15 00:20:28 -0400

    Draw Grid

--- Project3/Project3.py
+++ Project3/Project3.py
@@ -2,25 +2,152 @@
 
 import scanner
 import parser
+from collections import deque
+import turtle
+
 class Node:
-    def __init__(self, data = []): 
+    def __init__(self, data): 
         self.data = data
 
 class Project3:
     def createTree(structures):
         STstack = []
-        for (x,y) in structures:
-            if z == 3:      # two non-terminals in RHS
+        for (x,y,z) in structures:
+            #print(x,y,z)
+            if z == 0:
                 pass
-                # TODO: create node of type "seq" containing references to the two syntax trees
-            if z == 0 or z == 2 or z == 4 or z == 5 or z == 10:     # one non-terminal in RHS
+            if z == 1:
+                L = STstack.pop()
+                node = Node(y[0:4] + [L] + [y[-1]])
+                STstack.append(node)
+            if z == 2:
+                pass    # do nothing
+            if z == 3:
+                S = STstack.pop()
+                L = STstack.pop()
+                node = Node(['seq'] + [L] + [S])
+                STstack.append(node)
+            if z == 4:
+                D = STstack.pop()
+                node = Node(y[0:5] + [D])
+                STstack.append(node)
+            if z == 5:
+                D = STstack.pop()
+                node = Node(y[0:5] + [D])
+                STstack.append(node)
+            if z == 6:
+                node = Node(y)
+                STstack.append(node)
+            if z == 7:
+                node = Node(y + ['1'])
+                STstack.append(node)
+            if z == 8:
+                node = Node(y)
+                STstack.append(node)
+            if z == 9:
+                node = Node(y)
+                STstack.append(node)
+            if z == 10:
+                L = STstack.pop()
+                node = Node(y[0:2] + [L] + [y[-1]])
+                STstack.append(node)
+            if z == 11:
+                node = Node(['D'] + y)
+                STstack.append(node)
+            if z == 12:
+                node = Node(['D'] + y)
+                STstack.append(node)
+            if z == 13:
+                node = Node(['D'] + y)
+                STstack.append(node)
+            if z == 14:
+                node = Node(['D'] + y)
+                STstack.append(node)
+        return STstack
+
+    def traverseTree(tree):
+        def createGrid(i,j):
+            sc=turtle.Screen()
+            trtl=turtle.Turtle()
+            # method to draw y-axis lines
+            def drawy(val):
+                # line
+                trtl.forward(j*10)
+                # set position
+                trtl.up()
+                trtl.setpos(-300+val,-300)
+                trtl.down()
+            # method to draw y-axis lines
+            def drawx(val):
+                # line
+                trtl.forward(i*10)
+                # set position
+                trtl.up()
+                trtl.setpos(-300,-300+val)
+                trtl.down()
+
+            # Main Section
+            # set screen
+            sc.setup(800,800)    
+            # set turtle features
+            trtl.speed(100)
+            trtl.up()
+            trtl.setpos(-300,-300)
+            trtl.down()
+            trtl.left(90)  
+            trtl.color('lightgreen')
+            # y lines
+            for y in range(i+1):
+                drawy(10*(y+1))
+            # set position for x lines
+            trtl.right(90)
+            trtl.up()
+            trtl.setpos(-300,-300)
+            trtl.down()
+            # x lines
+            for x in range(j+1):
+                drawx(10*(x+1))
+            turtle.Screen().exitonclick()
+            
+        def placeCat(cat,i,j):
+            return 0
+        def placeMouse(mouse,i,j):
+            return 0
+        def placeHole(i,j):
+            return 0
+        
+        q = deque()
+        q.append(tree[0])
+        while q:
+            node = q.popleft()
+            nodeType = node.data[0]
+            if nodeType == 'size':
+                i = int(node.data[1])
+                j = int(node.data[2])
+                createGrid(i,j)
+            if nodeType == 'cat':
+                cat = node.data[1]
+                i = int(node.data[2])
+                j = int(node.data[3])
+                #placeCat(cat,i,j)
+            if nodeType == 'mouse':
+                mouse = node.data[1]
+                i = int(node.data[2])
+                j = int(node.data[3])
+                #placeMouse(mouse,i,j)
+            if nodeType == 'hole':
+                i = int(node.data[1])
+                j = int(node.data[2])
+                #placeHole(i,j)
+            if nodeType == 'seq':
                 pass
-            else:
-                y[0] = Node(y[1:])
-                STstack.append(y[0])
-                
-
-
+            if nodeType == 'move':
+                pass
+            if nodeType == 'clockwise':
+                pass
+            if nodeType == 'repeat':
+                pass
+        return
 
 if __name__ == "__main__":
     # create table
@@ -40,4 +167,7 @@ if __name__ == "__main__":
     if structures == None:
         print('CATANDMOUSE program is not syntactically correct')
     else:
-        parser.printOutput(structures)
+        tree = Project3.createTree(structures)
+        Project3.traverseTree(tree)
+
+        
